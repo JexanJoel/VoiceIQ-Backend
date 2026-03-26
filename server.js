b7a30c0482647061ecd9284967969be9ef560f67
Jexan Joel <jexanjoel1523@gmail.com>
2026-03-26 12:55:34 +0530

    26-03-2026 x3

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'https://voice-iq-five.vercel.app',
+  origin: 'https://voice-iq-ai.vercel.app',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
