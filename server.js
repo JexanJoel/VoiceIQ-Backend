1a5036459741c041dc47116bf3e064bc9feb20b4
Jexan Joel <jexanjoel1523@gmail.com>
2026-04-02 11:30:39 +0530

    Updated server.js

--- server.js
+++ server.js
@@ -21,10 +21,10 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
-app.use("/api/call-analytics", callAnalyticsRoute);
-
 app.options('*', cors());
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/call-analytics", callAnalyticsRoute);
 
 app.get("/", (req, res) => {
   res.json({ status: "VoiceIQ Backend Running 🎙️" });
